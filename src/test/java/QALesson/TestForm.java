91ed5fad36c37853f5def563201a06e30a706c01
Luck <IlyaT123@yandex.ru>
2022-09-24 20:47:08 +0300

    Добавил проверку формы

--- src/test/java/QALesson/TestForm.java
+++ src/test/java/QALesson/TestForm.java
@@ -3,6 +3,7 @@ package QALesson;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.JavascriptExecutor;
@@ -39,6 +40,27 @@ public class TestForm {
         $x("//*[text() = 'Delhi']").scrollIntoView(true).click();
         $("#submit").submit();
         $x("//*[. = 'Thanks for submitting the form']").should(exist);
-    }
 
+        Assertions.assertEquals($x("//td[.='Student Name']/following-sibling::td").getText(),"Тапок Котофей");
+        Assertions.assertEquals($x("//td[.='Student Email']/following-sibling::td").getText(),"TapokKoTof@yandex.ru");
+        Assertions.assertEquals($x("//td[.='Gender']/following-sibling::td").getText(),"Male");
+        Assertions.assertEquals($x("//td[.='Mobile']/following-sibling::td").getText(),"8342546385");
+        Assertions.assertEquals($x("//td[.='Date of Birth']/following-sibling::td").getText(),"24 September,2022");
+        Assertions.assertEquals($x("//td[.='Subjects']/following-sibling::td").getText(),"English");
+        Assertions.assertEquals($x("//td[.='Hobbies']/following-sibling::td").getText(),"Music");
+        Assertions.assertEquals($x("//td[.='Picture']/following-sibling::td").getText(),"cat.jpg");
+        Assertions.assertEquals($x("//td[.='Address']/following-sibling::td").getText(),"Москва, колбасный завод");
+        Assertions.assertEquals($x("//td[.='State and City']/following-sibling::td").getText(),"NCR Delhi");
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
